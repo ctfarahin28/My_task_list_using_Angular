67551cacb08127c1de3274c28382f6949c8a7297
ctfarahin28 <ctfarahin28@gmail.com>
2022-08-19 16:25:00 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, VERSION } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
@@ -8,14 +8,22 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 })
 export class AppComponent {
   taskForm: FormGroup; // step 3 define formgroup
-
+  items: FormArray;
   //step 4 instance of form builder construcor
 
   constructor(private formBuilder: FormBuilder) {
     //create formbuilder for FormGroup and group the formControl
+    // this.taskForm = formBuilder.group({
+    //   task: new FormControl(),
+    //   desc: new FormControl(),
+
     this.taskForm = formBuilder.group({
-      task: new FormControl(),
-      desc: new FormControl(),
+      items: formBuilder.array([
+        this.formBuilder.group({
+          task: ['p'],
+          desc: ['s'],
+        }),
+      ]),
     });
   }
 }
